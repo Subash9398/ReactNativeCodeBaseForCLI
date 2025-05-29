e40bb70971504d2813d74e6f505656dc7e039942
Subash <subash.sekar@tigeranalytics.com>
2025-05-29 13:14:02 +0530

    initial Commit

---
+++ e2e/starter.test.ts
@@ -0,0 +1,27 @@
+import { by, device, expect, element } from 'detox';
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('shows the welcome screen', async () => {
+    await expect(element(by.text('Debug'))).toBeVisible();
+  });
+
+  it('renders the text below the fold', async () => {
+    await expect(element(by.text('The Basics'))).toExist();
+  });
+
+  it('scrolls to the link and opens it', async () => {
+    await waitFor(element(by.text('Style')))
+      .toBeVisible()
+      .whileElement(by.id('ScrollView'))
+      .scroll(300, 'down');
+    await element(by.text('Style')).tap();
+  });
+});
